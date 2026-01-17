8227f02682514f20093dc5ab865eb403f79996a2
EmanuelBachmann <emanuel@familie-bachmann.at>
2026-01-17 11:10:07 +0100

    Add cookiebot

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,13 @@ class MyDocument extends Document {
             src='https://kit.fontawesome.com/61f1b30c9d.js'
             crossOrigin='anonymous'
           ></script>
+          <script
+            id='Cookiebot'
+            src='https://consent.cookiebot.com/uc.js'
+            data-cbid='13ce34f7-5e85-49bc-a5c9-56c3ec425c20'
+            data-blockingmode='auto'
+            type='text/javascript'
+          ></script>
         </Head>
         <body>
           <Main />
