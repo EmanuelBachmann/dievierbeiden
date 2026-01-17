2a2791d4e4eae0463b5ac4825e7b94f8af2fa1e0
EmanuelBachmann <emanuel@familie-bachmann.at>
2026-01-17 09:43:39 +0100

    enable recaptcha

--- components/global/contact.jsx
+++ components/global/contact.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import data from '../../lib/data';
 import MarkdownIt from 'markdown-it';
+import { useRouter } from 'next/router';
 const md = new MarkdownIt({ html: true });
 
 export default function Introduction({ block, dataBinding }) {
+  const router = useRouter();
+  const submittedQuery = router.query.submitted;
+  const [submitted, setSubmitted] = useState(false);
+  const [tokenCreated, setTokenCreated] = useState(false);
+  const formEl = useRef(null);
+
+  useEffect(() => {
+    if (!submittedQuery) {
+      const script = document.createElement('script');
+      script.src =
+        'https://www.google.com/recaptcha/api.js?render=' +
+        process.env.NEXT_PUBLIC_RECAPTCHA_KEY;
+      script.addEventListener('load', setupForm);
+      document.body.appendChild(script);
+
+      return () => {
+        script.removeEventListener('load', setupForm);
+        document.body.removeChild(script);
+      };
+    }
+  }, []);
+
+  const setupForm = () => {
+    formEl.current.addEventListener('submit', function (event) {
+      if (!tokenCreated) {
+        event.preventDefault();
+        if (!submitted) {
+          setSubmitted(true);
+          grecaptcha.ready(function () {
+            grecaptcha
+              .execute(process.env.NEXT_PUBLIC_RECAPTCHA_KEY, {
+                action: 'create_comment',
+              })
+              .then(function (token) {
+                const input = document.createElement('input');
+                input.type = 'hidden';
+                input.name = 'g-recaptcha-response';
+                input.value = token;
+                formEl.current.appendChild(input);
+                setTokenCreated(true);
+                formEl.current.submit();
+              });
+          });
+        }
+      }
+    });
+  };
   return (
     <section className='contact' data-cms-bind={dataBinding} id={block.anchor}>
       <div className='contact-content'>
@@ -25,32 +73,39 @@ export default function Introduction({ block, dataBinding }) {
           </div>
         </div>
 
-        <form className='contact-form container-md'>
-          <input type='hidden' name='inbox_key' value='dievierbeiden-contact' />
+        <form
+            method='post'
+            className='contact-form container-md'
+            action='?submitted=true'
+            id='contact-form'
+            ref={formEl}
+          >
           <div className='form-group'>
             <label htmlFor='name'>Vorname</label>
-            <input type='text' id='name' name='name' />
+            <input type='text' id='name' name='name' required />
           </div>
 
           <div className='form-group'>
             <label htmlFor='name'>Nachname</label>
-            <input type='text' id='name' name='name' />
+            <input type='text' id='name' name='name' required />
           </div>
 
           <div className='form-group'>
             <label htmlFor='email'>E-Mail</label>
-            <input type='email' id='email' name='email' />
+            <input type='email' id='email' name='email' required />
           </div>
 
           <div className='form-group'>
             <label htmlFor='phone'>Telefonnummer</label>
-            <input type='tel' id='phone' name='phone' />
+            <input type='tel' id='phone' name='phone' required />
           </div>
 
           <div className='form-group col-span'>
             <label htmlFor='message'>Nachricht</label>
-            <textarea id='message' name='message' rows='5'></textarea>
+            <textarea id='message' name='message' rows='5' required></textarea>
           </div>
+
+          <input type='hidden' name='inbox_key' value='dievierbeiden-contact' />
 
           <button type='submit' className='contact-submit col-span'>
             Senden
